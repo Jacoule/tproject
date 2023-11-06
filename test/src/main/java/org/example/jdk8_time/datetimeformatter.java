2bb06883b1166be5bed747a45b810f09b24169b6
何子孝 <1079049205@qq.com>
2023-11-06 14:44:39 +0800

    11月6 LocalDate LocalTime LocalDateTime datetimeformatter

--- test/src/main/java/org/example/jdk8_time/datetimeformatter.java
+++ test/src/main/java/org/example/jdk8_time/datetimeformatter.java
@@ -7,7 +7,8 @@ import java.time.format.DateTimeFormatter;
 
 public class datetimeformatter {
     public static void main(String[] args) {
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy年MM月dd日 HH:mm:ss");
+        DateTimeFormatter dateTimeFormatter =
+                DateTimeFormatter.ofPattern("yyyy年MM月dd日 HH:mm:ss");
         LocalDateTime now = LocalDateTime.now();
 
         //反向格式化
@@ -20,6 +21,6 @@ public class datetimeformatter {
 
         String time = "2023年11月05日 08:42:17";
         LocalDateTime parse = LocalDateTime.parse(time, dateTimeFormatter);
-        System.out.println(parse);
+        System.out.println(parse.format(dateTimeFormatter));
     }
 }
